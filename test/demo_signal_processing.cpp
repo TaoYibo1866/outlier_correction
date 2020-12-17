6085b9debb29e27c6cd82780e06dbbecde353475
TaoYibo <821869740@qq.com>
2020-12-17 20:38:44 +0800

    signal processing test

--- test/demo_signal_processing.cpp
+++ test/demo_signal_processing.cpp
@@ -6,7 +6,7 @@ using correct_outlier::findOutlier;
 using correct_outlier::correctOutlier;
 using std::vector;
 
-int readFile(const char* filename, vector<double> signal)
+int readFile(const char* filename, vector<double>& signal)
 {
     std::fstream file;
     file.open(filename, std::ios::in);
@@ -19,6 +19,20 @@ int readFile(const char* filename, vector<double> signal)
         std::sscanf(tp.c_str(), "%lf", &data);
         signal.push_back(data);
     }
+    file.close();
+    return 0; 
+}
+
+int writeFile(const char* filename, vector<double> signal)
+{
+    std::fstream file;
+    file.open(filename, std::ios::trunc | std::ios::out);
+    if (file.is_open() != true) return -1;
+    for (int i = 0; i < signal.size(); i++)
+    {
+        file << signal[i] << std::endl;
+    }
+    file.close();
     return 0; 
 }
 
@@ -27,10 +41,8 @@ int main()
     vector<double> input, output;
     if (readFile("../test/input.csv", input) == -1)
     {
-        printf("fail open file\n");
+        printf("fail open input file\n");
     }
-    vector<int> inliers, outliers;
-    double k, b;
     int len = 20;
     vector<double> cache(len), _cache(len);
     for (int i = 0; i < input.size(); i++)
@@ -42,9 +54,15 @@ int main()
         }
         cache[len - 1] = input[i];
         _cache[len - 1] = input[i];
+        double k, b;
+        vector<int> inliers, outliers;
         findOutlier(_cache, inliers, outliers, k, b, 2, 1);
         correctOutlier(cache, inliers, outliers);
         output.push_back(cache.back());
+    }
+    if (writeFile("../test/output.csv", output) == -1)
+    {
+        printf("fail open output file\n");
     }
     return 0;
 }
